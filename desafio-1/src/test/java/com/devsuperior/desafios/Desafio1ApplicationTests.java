0d8766bbe1c9a89cf2b7494fbfecb82209642df7
Lopes, Maycon <lopes.maycon@gmail.com>
2025-01-25 07:14:25 -0300

    Initial commit and Order entity implementation

---
+++ desafio-1/src/test/java/com/devsuperior/desafios/Desafio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
